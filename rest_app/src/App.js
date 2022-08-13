e5a93f3eebceddf7cca5b9c9223c95e736be598b
unknown <Mohd-naim>
2022-08-14 01:33:03 +0530

    Add the footer
    I have added the footer

--- rest_app/src/App.js
+++ rest_app/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import Restorent from "./component/basics/Restorent";
+import "./footer.css"
 const App = () => {
-  return <Restorent/>
+  return <>
+  <Restorent/>
+  <Footer/>
+  </>
+
+}
+const Footer =()=>{
+  
+ return<>
+  <div className="footer">
+    <h2>Made By <a href="http://" target="_blank" rel="noopener noreferrer">Mohd Naim</a> with ❤️</h2>
+    <h3>All right reserved &copy; 2022-23</h3>
+  </div>
+ </>
 }
 
 
