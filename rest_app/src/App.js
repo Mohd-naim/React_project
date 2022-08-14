4c4dc54bfb6fd52a0cfe92a4c65f5cda9498941d
Mohd-naim <98208008+Mohd-naim@users.noreply.github.com>
2022-08-14 16:32:40 +0530

    add link
    i have added my portfolio link

--- rest_app/src/App.js
+++ rest_app/src/App.js
@@ -12,7 +12,7 @@ const Footer =()=>{
   
  return<>
   <div className="footer">
-    <h2>Made By <a href="http://" target="_blank" rel="noopener noreferrer">Mohd Naim</a> with ❤️</h2>
+    <h2>Made with ❤️ BY <a href="https://mohd-naim.github.io/mohdnaim.github.io/" target="_blank" rel="noopener noreferrer">Mohammad Naim</a></h2>
     <h3>All right reserved &copy; 2022-23</h3>
   </div>
  </>
