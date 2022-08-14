5234216ebe1affbb41419ef1a0669eebaf3eee22
Mohd-naim <98208008+Mohd-naim@users.noreply.github.com>
2022-08-14 08:10:19 +0530

    My first react project
    This is my first react project

---
+++ React_first/rest_app/src/component/basics/menuApi.js
@@ -0,0 +1,96 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://im.rediff.com/getahead/2020/sep/29burnt-garlic-chilli-maggi.jpg?w=670&h=900",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://kfoods.com/images1/newrecipeicon/aloo-pakora_2421.jpg",
+      name: "aalu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://www.thespruceeats.com/thmb/2sfb68C3oVFG2wLngCzo1_Kx6PY=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/steamed-corn-on-the-cob-2138219-hero-01-8ba7ab5f6a2f42338ea73e707228b15c.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://thumbs.dreamstime.com/b/chickpeas-masala-spicy-chola-chhole-curry-bhatura-puri-garnished-fresh-green-coriander-ingredients-classic-indian-223500323.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-homemade-pizza-horizontal-1542312378.png?crop=1.00xw:1.00xh;0,0&resize=980:*",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://thumbs.dreamstime.com/z/indian-non-veg-thali-food-platter-consists-variety-veggies-chicken-meat-lentils-sweet-dish-snacks-etc-selective-focus-225928502.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://1.bp.blogspot.com/-Yn52GtCFZas/X6ft5y-LO3I/AAAAAAAAStI/sq87NFdMRyYH_g_8Vrg2_LZkMa-HPtHTACLcBGAsYHQ/s16000/Diwali%2BSweets.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://www.themealbowl.com/tmb_admin/productimages/22/Mattar%20paneer%20rice%20bowl_0516.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Samosachutney.jpg/420px-Samosachutney.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+
+    {
+        id: 10,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Samosachutney.jpg/420px-Samosachutney.jpg",
+        name: "samaso",
+        category: "Sham",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+  ];
+  
+  export default Menu;
